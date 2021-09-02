84544e15676700f4415d6578944b3312bddc1c16
SQRV <89662891+SQRV@users.noreply.github.com>
2021-09-02 03:05:18 +0300

    Add files via upload

---
+++ SQRV_tube.py
@@ -0,0 +1,27 @@
+# warna
+h="\033[32;1m"
+
+# module
+import pytube
+import os,sys,time,requests
+from time import sleep
+# tampilan
+os.system("clear")
+sleep(1)
+os.system("toilet -f future sqrv tube | lolcat")
+sleep(1)
+banner= h+"""
+[•]───────────────────────────────────────────[•]
+          You can download the video
+             easily from SQRV Tube
+[•]───────────────────────────────────────────[•]  """
+print(banner)
+sleep(1)
+print('Download video from Youtube')
+sleep(1)
+url = input ('Enter video url : ')
+sleep(1)
+print('loading...')
+pytube.YouTube(url).streams.get_highest_resolution().download('/sdcard/SQEV_tube')
+print('Done')
+
